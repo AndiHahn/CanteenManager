d4f72e80607f8f8d8b247f2c6229a5b2f2b92f67
Andreas Hahn <HAHNAN>
2020-01-01 16:12:48 +0100

    finished map

--- app/src/main/java/com/example/canteenchecker/canteenmanager/ui/MapActivity.java
+++ app/src/main/java/com/example/canteenchecker/canteenmanager/ui/MapActivity.java
@@ -1,9 +1,5 @@
 package com.example.canteenchecker.canteenmanager.ui;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
@@ -11,16 +7,20 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.canteenchecker.canteenmanager.R;
-import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -30,16 +30,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.util.List;
 
-public class MapActivity extends AppCompatActivity implements LocationListener {
+public class MapActivity extends AppCompatActivity {
 
     //constants
     private static final String TAG = "MapActivity";
-    public static final String ADDRESS_KEY = "Address" ;
-    private static final long MIN_TIME = 400;
-    private static final float MIN_DISTANCE = 200;
+    public static final String ADDRESS_KEY = "Address";
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
 
     //layout
@@ -47,9 +44,14 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
     private FloatingActionButton btnSaveLocation;
     private FloatingActionButton btnNavigateToCurPos;
 
-    private LocationManager locationManager;
-
     private String address;
+    private GoogleMap googleMap;
+    private Geocoder geocoder;
+    private Marker markerCenter;
+    private boolean mapMovedByUser;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
 
     public static Intent createIntent(Context context, String address) {
         Intent intent = new Intent(context, MapActivity.class);
@@ -65,25 +67,73 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
         // get address from CanteenFragment
         Intent intent = getIntent();
         address = intent.getStringExtra(ADDRESS_KEY);
-        Log.e("FUD", "Address passed to MapActivity: " + address);
+        geocoder = new Geocoder(this);
 
         mpfMap = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mpfMap);
         btnSaveLocation = findViewById(R.id.btnSaveLocation);
         btnSaveLocation.setOnClickListener(v -> saveLocation());
         btnNavigateToCurPos = findViewById(R.id.btnNavigateToCurPos);
-        btnNavigateToCurPos.setOnClickListener(v -> {
-            Log.e("TEST", "clicked button location current");
-            navigateToCurrentLocation();
-        });
+        btnNavigateToCurPos.setOnClickListener(v -> navigateToCurrentLocation());
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLocation();
 
         updateMap();
     }
 
     private void saveLocation() {
+        getCurrentMarkerAddress();
         Intent intent = new Intent(this, MapActivity.class);
         intent.putExtra(ADDRESS_KEY, this.address);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    private void getCurrentMarkerAddress() {
+        if (markerCenter != null && mapMovedByUser) {
+            LatLng position = markerCenter.getPosition();
+            try {
+                List<Address> addresses = geocoder.getFromLocation(position.latitude, position.longitude, 1);
+                if(addresses != null && addresses.size() > 0 ) {
+                    this.address = addresses.get(0).getAddressLine(0);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Geocoding for current marker position failed", e);
+            }
+        }
+    }
+
+    private void fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                }
+            }
+        });
+    }
+
+    private void navigateToCurrentLocation() {
+        if (this.currentLocation == null) {
+            fetchLocation();
+        }
+
+        if (this.googleMap != null && this.currentLocation != null) {
+            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            btnNavigateToCurPos.setImageResource(R.drawable.ic_gps_fixed_24px);
+            mapMovedByUser = true;
+        }
     }
 
     private void updateMap() {
@@ -93,7 +143,6 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
             protected LatLng doInBackground(String... params) {
                 //try to return canteen location (string -> lat, long)
                 LatLng location = null;
-                Geocoder geocoder = new Geocoder(MapActivity.this);
                 try {
                     List<Address> addresses = params[0] == null ? null : geocoder.getFromLocationName(params[0], 1);
                     if(addresses != null && addresses.size() > 0) {
@@ -111,74 +160,63 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
 
             @Override
             protected void onPostExecute(final LatLng location) {
-                //update map racording to our location
+                //update map according to our location
                 mpfMap.getMapAsync(new OnMapReadyCallback() {
                     @Override
                     public void onMapReady(GoogleMap googleMap) {
+                        MapActivity.this.googleMap = googleMap;
+
                         googleMap.clear();
                         if(location != null) {
-                            googleMap.addMarker(new MarkerOptions().position(location));
                             googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
                         } else {
                             navigateToCurrentLocation();
-                            //googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(0,0),0));
                         }
+
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(googleMap.getCameraPosition().target);
+                        markerCenter = googleMap.addMarker(markerOptions);
+
+                        mapMovedByUser = false;
+
+                        googleMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+                            public void onCameraMove() {
+                                markerCenter.setPosition(googleMap.getCameraPosition().target);
+                            }
+                        });
+
+                        googleMap.setOnCameraMoveStartedListener(new GoogleMap.OnCameraMoveStartedListener() {
+                            @Override
+                            public void onCameraMoveStarted(int reason) {
+                                if (reason == GoogleMap.OnCameraMoveStartedListener.REASON_GESTURE) {
+                                    //The user gestured on the map.
+                                    mapMovedByUser = true;
+                                    btnNavigateToCurPos.setImageResource(R.drawable.ic_gps_not_fixed_24px);
+                                } else if (reason == GoogleMap.OnCameraMoveStartedListener
+                                        .REASON_API_ANIMATION) {
+                                    //The user tapped something on the map.
+                                    mapMovedByUser = true;
+                                    btnNavigateToCurPos.setImageResource(R.drawable.ic_gps_not_fixed_24px);
+                                } else if (reason == GoogleMap.OnCameraMoveStartedListener
+                                        .REASON_DEVELOPER_ANIMATION) {
+                                    //The app moved the camera.
+                                }
+                            }
+                        });
                     }
                 });
             }
         }.execute(address);
     }
 
-    private void navigateToCurrentLocation() {
-        //check if permission for making a phone call has been granted
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //request permission
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-        } else {
-            //read current location
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
-            //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER
-        }
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                navigateToCurrentLocation();
-            } else {
-                Toast.makeText(this, "No location permission", Toast.LENGTH_SHORT).show();
-            }
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation();
+                }
+                break;
         }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 12);
-        mpfMap.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                googleMap.animateCamera(cameraUpdate);
-                locationManager.removeUpdates(MapActivity.this);
-                btnNavigateToCurPos.setImageResource(R.drawable.ic_gps_fixed_24px);
-            }
-        });
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
     }
 }
